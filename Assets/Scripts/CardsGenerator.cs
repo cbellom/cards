79836990e4e0bfd873920759b079b2949f7a6c1f
cbellom <cbellom@unal.edu.co>
2015-12-02 11:18:50 -0500

    shuffle cards

--- Assets/Scripts/CardsGenerator.cs
+++ Assets/Scripts/CardsGenerator.cs
@@ -8,26 +8,67 @@ public class CardsGenerator : MonoBehaviour {
     public Data data;
 
     void Start() {
-        CreateCouplesCards(data.listCards);
+        List<GameObject> cards = CreateCouplesCards(data.listCards);
+        cards = ShuffleCards(cards);
+        InsertCardsIntoGrid(cards);
     }
 
-    void CreateCouplesCards(List<CoupleCards> listCoupleCards)
+    private List<GameObject> CreateCouplesCards(List<CoupleCards> listCoupleCards)
     {
+        List<GameObject> cards = new List<GameObject>();
         foreach (CoupleCards coupleCard in listCoupleCards)
         {
-            CreateCard(coupleCard.firstCard);
-            CreateCard(coupleCard.secondCard);
+            CreateCouple(cards, coupleCard);
         }
+        return cards;
     }
 
-    private void CreateCard(Card card)
+    private void CreateCouple(List<GameObject> cards, CoupleCards coupleCard)
+    {
+        GameObject firstCard = CreateCard(coupleCard.firstCard);
+        GameObject secondCard = CreateCard(coupleCard.secondCard);
+        SetMatchingCards(firstCard, secondCard);
+        cards.Add(firstCard);
+        cards.Add(secondCard);
+    }
+
+    private GameObject CreateCard(Card card)
     {
         Vector3 position = cardsGrid.transform.position;
         GameObject newCard = Instantiate(cardPrefab, position, Quaternion.identity) as GameObject;
-        newCard.transform.SetParent(cardsGrid.gameObject.transform, false);
 
         CardController cardController = newCard.GetComponent<CardController>();
         cardController.SetCard(card);
-        
+        return newCard;
+    }
+
+    private void SetMatchingCards(GameObject firstCard, GameObject secondCard)
+    {
+        CardController cardController = firstCard.GetComponent<CardController>();
+        cardController.match = secondCard;
+
+        cardController = secondCard.GetComponent<CardController>();
+        cardController.match = firstCard;
+    }
+
+    private List<GameObject> ShuffleCards(List<GameObject> cards)
+    {
+        List<GameObject> randomCards = new List<GameObject>();
+        while (cards.Count != 0)
+        {
+            GameObject g = cards[Random.Range(0, cards.Count)];
+            cards.Remove(g);
+            randomCards.Add(g);
+        }
+
+        return randomCards;
+    }
+
+    private void InsertCardsIntoGrid(List<GameObject> cards)
+    {
+        foreach (GameObject card in cards)
+        {
+            card.transform.SetParent(cardsGrid.gameObject.transform, false);
+        }
     }
 }
